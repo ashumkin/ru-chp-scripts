e7df56abd65e8dcfc2317f7777f8249f9f945ee0
Alexey Shumkin <Alex.Crezoff@gmail.com>
2012-07-06 13:05:23 +0400

    + User script from LJ-user "chabapok" for Google Chrome & Firefox+Greasemonkey: RU_CHP.user.js 	downloaded from http://www.alk.lv/RU_CHP.user.js 	Opera usable, too

---
+++ RU_CHP.user.js
@@ -0,0 +1,41 @@
+﻿// ==UserScript==
+// @name           RU_CHP
+// @namespace      http://ru_chp.livejournal.com/
+// @description    improve video view in ru_chp
+// @include        http://ru-chp.livejournal.com/*
+// @exclude        http://ru-chp.livejournal.com/ru_chp/*
+// ==/UserScript==
+
+//alert(document.location);
+
+text =
+'<input type="hidden" name="journal" value="ru_chp">' +
+'<fieldset>' +
+'<input type="text" name="url" id="ruchpPostUrl" readonly="readonly" class="text" value="'+document.location+'">' +
+'</fieldset>' +
+'<fieldset>' +
+'<input type="submit" class="submit" alt="Ok"> ' +
+'</fieldset>';
+
+
+
+
+
+var lj_controlstrip_new = document.getElementById("lj_controlstrip_new");
+
+var form = document.createElement("form");
+form.setAttribute("class", "w-cs-search");
+form.setAttribute("action", "http://www.alk.lv/ruchp");
+form.innerHTML=text;
+
+
+for( var elem = lj_controlstrip_new.firstElementChild; elem!=null; elem=elem.nextElementSibling ){    
+        grp = elem.getAttribute("class");
+        if ( grp == "w-cs-user-controls"){
+			elem.appendChild(form);
+			break;
+        }
+}
+
+
+
