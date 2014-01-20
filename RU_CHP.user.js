29549da411bd0a67ca9cee3c34527ada22cd5df4
Alexey Shumkin <Alex.Crezoff@gmail.com>
2014-01-20 15:02:02 +0400

    improve: add links for Youtube
    It is convenient to have a button to open video on Youtube with start
    time defined in a post.

--- RU_CHP.user.js
+++ RU_CHP.user.js
@@ -2,7 +2,8 @@
 // @name           RU_CHP
 // @author         Alexey Shumkin aka Zapped
 // @license        GPL
-// @version        0.0.5.2
+// @version        0.0.6
+// @history        0.0.6 - Added Youtube field (to open video on Youtube with predefined time)
 // @history        0.0.5.2 - Fixed ?style=mine
 // @history        0.0.5.1 - Fixed variables error
 // @history        0.0.5 - Added support for Opera, removed alk.lv (invalid backup site long time ago)
@@ -46,6 +47,28 @@
 		append_form(element, rucrash_player_url, text);
 	}
 
+	function append_resolved_video(element, id_array) {
+		var id = id_array.id;
+		var time = id_array.time;
+		var time_str = '';
+		if (time != 0) {
+			time_str = '&amp;t=' + time + 's'
+		}
+		var youtube_site = 'http://www.youtube.com/watch?v=' + id + time_str;
+		text =
+			'<input type="text" size="40" readonly="readonly" class="text" value="' + youtube_site + '">' +
+			'<input type="hidden" name="v" value="' + id + '">' +
+			(time_str != '' ? '<input type="hidden" name="t" value="' + time + 's">' : '') +
+			'<input type="submit" class="submit" value="watch on Youtube" alt="Ok"> ';
+		append_form(element, youtube_site, text);
+	}
+
+	function append_resolved_videos(element, videos) {
+		for (var i = 0; i < videos.length; i++) {
+			append_resolved_video(element, videos[i]);
+		}
+	}
+
 	function inlined_videos_count(element) {
 		var videos = element.getElementsByTagName("iframe");
 		if (videos && videos.length != 0) {
@@ -59,6 +82,70 @@
 			}
 			return vcount;
 		}
+	}
+
+	function convert_to_seconds(match) {
+		var minutes = parseInt(match[1]);
+		var seconds = parseInt(match[2]);
+		seconds = 60 * minutes + seconds;
+		return seconds;
+	}
+
+	function extract_times(element, videos) {
+		var times_re = /(\d{1,2})[:.-](\d{2})/;
+		var body_entry = element.getElementsByClassName('entry-content');
+		if (body_entry && body_entry[0]) {
+			body_entry = body_entry[0].textContent;
+		} else {
+			// ?style=mine
+			body_entry = element.getElementsByClassName('b-singlepost-body')
+			if (body_entry && body_entry[0]) {
+				body_entry = body_entry[0].textContent;
+			}
+		}
+		var match = null;
+		var count = 0;
+		if (match = times_re.exec(body_entry)) {
+			var time = convert_to_seconds(match);
+			if (videos[count]) {
+				videos[count].time = time;
+			}
+		}
+		return videos;
+	}
+
+	function extract_youtube_url(src) {
+		src = unescape(src);
+		params = src.split('&');
+		var id;
+		for (var i = 0; i < params.length; i++) {
+			key_value = params[i].split('=');
+			if (key_value[0] == 'vid') {
+				id = key_value[1];
+				break;
+			}
+		}
+		return id;
+	}
+
+	function resolve_video_url(frame) {
+		var result = { id: extract_youtube_url(frame.src), time: 0};
+		return result;
+	}
+
+	function get_resolved_videos(frames) {
+		var result = new Array();
+		for (var i = 0; i < frames.length; i++) {
+			result[i] = resolve_video_url(frames[i]);
+		}
+		return result;
+	}
+
+	function get_inlined_videos(element) {
+		var videos = element.getElementsByTagName("iframe");
+		videos = get_resolved_videos(videos);
+		videos = extract_times(element, videos);
+		return videos;
 	}
 
 	function find_id(element, entry) {
@@ -99,6 +186,8 @@
 			article = entry.getElementsByClassName('b-singlepost-wrapper')[0]
 			if (article) {
 				append_forms_by_id(article, id);
+				videos = get_inlined_videos(article);
+				append_resolved_videos(article, videos);
 			}
 		} else {
 			entries = doc.getElementsByTagName('div');
@@ -112,6 +201,8 @@
 				}
 				if (id) {
 					append_forms_by_id(entry, id);
+					videos = get_inlined_videos(entry);
+					append_resolved_videos(entry, videos);
 				}
 			}
 		}
@@ -135,6 +226,8 @@
 						// find post text
 						id = id.replace("post-ru_chp-", "");
 						append_forms_by_id(entry, id);
+						videos = get_inlined_videos(entry);
+						append_resolved_videos(entry, videos);
 					}
 				}
 			}
